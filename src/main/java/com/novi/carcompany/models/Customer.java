460690b0527ec938f63b74585fc82f7cadf82ef1
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-09 23:33:05 +0200

    made complete customer model

--- src/main/java/com/novi/carcompany/models/Customer.java
+++ src/main/java/com/novi/carcompany/models/Customer.java
@@ -1,4 +1,49 @@
 package com.novi.carcompany.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customerd")
 public class Customer implements Person {
+
+    //TODO: set inheritance? annotation?
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "bankAccount")
+    private String bankAccount;
+    @Column(name = "corporate")
+    private Boolean corporate;
+    //TODO: add Car (foreign key)
+
+
+    public Customer() {
+    }
+
+    public Customer(String bankAccount, Boolean corporate) {
+        this.bankAccount = bankAccount;
+        this.corporate = corporate;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getBankAccount() {
+        return this.bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public Boolean getCorporate() {
+        return this.corporate;
+    }
+
+    public void setCorporate(Boolean corporate) {
+        this.corporate = corporate;
+    }
 }
