f9bd6e5d3d09b43908e027da10b7a8a7913a4fc5
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-09 23:37:43 +0200

    made complete employee model

--- src/main/java/com/novi/carcompany/models/Employee.java
+++ src/main/java/com/novi/carcompany/models/Employee.java
@@ -1,4 +1,38 @@
 package com.novi.carcompany.models;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "empoyes")
 public class Employee implements Person {
+
+    //TODO: add inheritance? annotation?
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "function")
+    private String function;
+
+
+    public Employee() {
+    }
+
+    public Employee(String function) {
+        this.function = function;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getFunction() {
+        return this.function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
 }
