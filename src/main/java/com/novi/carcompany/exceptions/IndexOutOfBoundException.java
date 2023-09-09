c0640cd7bd3e66982643e89ff279ede7a8da2ed4
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-10 00:07:07 +0200

    made exceptions

---
+++ src/main/java/com/novi/carcompany/exceptions/IndexOutOfBoundException.java
@@ -0,0 +1,14 @@
+package com.novi.carcompany.exceptions;
+
+public class IndexOutOfBoundException extends RuntimeException {
+
+    public IndexOutOfBoundException() {
+
+        super();
+    }
+
+    public IndexOutOfBoundException(String message) {
+
+        super(message);
+    }
+}
