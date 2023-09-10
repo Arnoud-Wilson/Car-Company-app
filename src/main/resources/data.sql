1670cd789cbb9c26ce5e254af2b1f94ddfc861fe
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-10 13:21:08 +0200

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO cars(license_plate, brand, model, vin_number, color, engine, winter_tyres)
+VALUES
+    ('nl-01-nl', 'Opel', 'Manta', 'VNV00012345678910',  'red', '2.0', false);
+
+
+
+
+INSERT INTO customers(sur_name, last_name, address, phone_number, bank_account, corporate)
+VALUES
+    ('Jan', 'Janssen', 'straat 1', 0100000000, 'NL01BANK00000', false),
+    ('Kees', 'knol', 'laan 2', 0200000000, 'NL02BANK00000', true);
