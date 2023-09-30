139413a3b3fcdd2c08cfb7a88fc7983b49e045a8
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-30 17:03:57 +0200

    added getParts service/controller

--- src/main/java/com/novi/carcompany/controllers/PartController.java
+++ src/main/java/com/novi/carcompany/controllers/PartController.java
@@ -1,4 +1,31 @@
 package com.novi.carcompany.controllers;
 
+import com.novi.carcompany.dtos.PartDto;
+import com.novi.carcompany.services.PartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parts")
 public class PartController {
+
+    private final PartService partService;
+
+    public PartController(PartService partService) {
+        this.partService = partService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<PartDto>> getCars() {
+
+        return ResponseEntity.ok(partService.getParts());
+    }
+
+
+
 }
--- src/main/java/com/novi/carcompany/services/PartService.java
+++ src/main/java/com/novi/carcompany/services/PartService.java
@@ -1,4 +1,49 @@
 package com.novi.carcompany.services;
 
+import com.novi.carcompany.dtos.CarDto;
+import com.novi.carcompany.dtos.PartDto;
+import com.novi.carcompany.exceptions.RecordNotFoundException;
+import com.novi.carcompany.helpers.DtoConverters;
+import com.novi.carcompany.models.Car;
+import com.novi.carcompany.models.Part;
+import com.novi.carcompany.repositories.PartRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PartService {
+
+    private final PartRepository partRepository;
+
+    public PartService(PartRepository partRepository) {
+        this.partRepository = partRepository;
+    }
+
+
+    ///// For fetching all parts in the database. /////
+    public List<PartDto> getParts(){
+        List<Part> fetchedParts = new ArrayList<>(partRepository.findAll());
+        List<PartDto> partDto = new ArrayList<>();
+
+        for (Part part : fetchedParts) {
+            PartDto dto = new PartDto();
+
+            DtoConverters.partDtoConverter(part, dto);
+
+            partDto.add(dto);
+        }
+        if (partDto.isEmpty()) {
+            throw new RecordNotFoundException("We hebben helaas geen onderdelen in onze database gevonden.");
+        } else {
+            return partDto;
+        }
+    }
+
+
+
+
+
 }
