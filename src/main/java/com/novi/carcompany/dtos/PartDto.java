0b65a35909aa5efc3c6e56f08fbdba77273a364a
Arnoud-Wilson <arnoud608@gmail.com>
2023-09-30 14:59:16 +0200

    made part dto's

--- src/main/java/com/novi/carcompany/dtos/PartDto.java
+++ src/main/java/com/novi/carcompany/dtos/PartDto.java
@@ -1,4 +1,13 @@
 package com.novi.carcompany.dtos;
 
 public class PartDto {
+
+    public String partNumber;
+    public String name;
+    public String description;
+    public String location;
+    public int stock;
+    public Double purchasePrice;
+    public Double sellingPrice;
+
 }
--- src/main/java/com/novi/carcompany/dtos/PartInputDto.java
+++ src/main/java/com/novi/carcompany/dtos/PartInputDto.java
@@ -1,4 +1,19 @@
 package com.novi.carcompany.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class PartInputDto {
+
+    @NotBlank
+    public String partNumber;
+    @NotBlank
+    public String name;
+    public String description;
+    public String location;
+    @NotBlank
+    public int quantity;
+    @NotBlank
+    public Double purchasePrice;
+    public Double sellingPrice;
+
 }
