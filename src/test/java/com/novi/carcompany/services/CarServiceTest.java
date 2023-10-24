359a99d9a642f983a08cf09295d173247e046ed6
Arnoud-Wilson <arnoud608@gmail.com>
2023-10-24 16:58:57 +0200

    added application-test properties and H2 database pom.xml

---
+++ src/test/java/com/novi/carcompany/services/CarServiceTest.java
@@ -0,0 +1,41 @@
+package com.novi.carcompany.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCars() {
+    }
+
+    @Test
+    void getCar() {
+    }
+
+    @Test
+    void findCarByVinNumber() {
+    }
+
+    @Test
+    void findCar() {
+    }
+
+    @Test
+    void createCar() {
+    }
+
+    @Test
+    void changeCar() {
+    }
+
+    @Test
+    void deleteCar() {
+    }
+}
