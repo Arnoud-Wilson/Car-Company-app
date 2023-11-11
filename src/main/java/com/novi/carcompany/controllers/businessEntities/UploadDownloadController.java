19b2ebb1a4f5bacc76073a1a95eaf941161ce6a1
Arnoud-Wilson <arnoud608@gmail.com>
2023-11-11 13:22:00 +0100

    completed upload/download controller/service

--- src/main/java/com/novi/carcompany/controllers/businessEntities/UploadDownloadController.java
+++ src/main/java/com/novi/carcompany/controllers/businessEntities/UploadDownloadController.java
@@ -14,8 +14,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
+@RequestMapping("/load")
 public class UploadDownloadController {
 
     private final UploadDownloadService uploadDownloadService;
@@ -25,21 +26,20 @@ public class UploadDownloadController {
     }
 
 
-    @PostMapping("single/uploadDb")
+    @PostMapping("single/upload")
     public FileUploadResponseDto singleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
 
 
         // next line makes url. example "http://localhost:8080/download/naam.jpg"
         FileDocument fileDocument = uploadDownloadService.uploadFileDocument(file);
-        String url = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFromDB/").path(Objects.requireNonNull(file.getOriginalFilename())).toUriString();
+        String url = ServletUriComponentsBuilder.fromCurrentContextPath().path("load//download/").path(Objects.requireNonNull(file.getOriginalFilename())).toUriString();
 
         String contentType = file.getContentType();
 
-        return new FileUploadResponseDto(fileDocument.getFileName(), url, contentType );
+        return new FileUploadResponseDto(fileDocument.getFileName(), contentType, url);
     }
 
-    //    get for single download
-    @GetMapping("/downloadFromDB/{fileName}")
+    @GetMapping("/download/{fileName}")
     ResponseEntity<byte[]> downLoadSingleFile(@PathVariable String fileName, HttpServletRequest request) {
 
         FileDocument document = uploadDownloadService.singleFileDownload(fileName, request);
@@ -48,26 +48,7 @@ public class UploadDownloadController {
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "inline;fileName=" + document.getFileName()).body(document.getDocFile());
     }
 
-    //    post for multiple uploads to database
-    @PostMapping("/multiple/upload/db")
-    List<FileUploadResponseDto> multipleUpload(@RequestParam("files") MultipartFile [] files) {
-
-        if(files.length > 7) {
-            throw new RuntimeException("to many files selected");
-        }
-
-        return uploadDownloadService.createMultipleUpload(files);
-
-    }
-
-    @GetMapping("zipDownload/db")
-    public void zipDownload(@RequestBody String[] files, HttpServletResponse response) throws IOException {
-
-        uploadDownloadService.getZipDownload(files, response);
-
-    }
-
-    @GetMapping("/getAll/db")
+    @GetMapping("/getAll")
     public Collection<FileDocument> getAllFromDB(){
         return uploadDownloadService.getALlFromDB();
     }
--- src/main/java/com/novi/carcompany/services/businessEntities/UploadDownloadService.java
+++ src/main/java/com/novi/carcompany/services/businessEntities/UploadDownloadService.java
@@ -1,25 +1,14 @@
 package com.novi.carcompany.services.businessEntities;
 
-import com.novi.carcompany.dtos.businessEntities.FileUploadResponseDto;
 import com.novi.carcompany.models.businessEntities.FileDocument;
-import com.novi.carcompany.models.businessEntities.Part;
 import com.novi.carcompany.repositories.DocFileRepository;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 
 @Service
@@ -43,102 +32,12 @@ public class UploadDownloadService {
         docFileRepository.save(fileDocument);
 
         return fileDocument;
-
     }
 
     public FileDocument singleFileDownload(String fileName, HttpServletRequest request){
 
         FileDocument document = docFileRepository.findByFileName(fileName);
 
-//        this mediaType decides witch type you accept if you only accept 1 type
-//        MediaType contentType = MediaType.IMAGE_JPEG;
-//        this is going to accept multiple types
-
-        String mimeType = request.getServletContext().getMimeType(document.getFileName());
-
-//        for download attachment use next line
-//        return ResponseEntity.ok().contentType(contentType).header(HttpHeaders.CONTENT_DISPOSITION, "attachment;fileName=" + resource.getFilename()).body(resource);
-//        for showing image in browser
         return document;
-
-    }
-
-    public List<FileUploadResponseDto> createMultipleUpload(MultipartFile[] files){
-        List<FileUploadResponseDto> uploadResponseList = new ArrayList<>();
-        Arrays.stream(files).forEach(file -> {
-
-            String name = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-            FileDocument fileDocument = new FileDocument();
-            fileDocument.setFileName(name);
-            try {
-                fileDocument.setDocFile(file.getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            docFileRepository.save(fileDocument);
-
-//            next line makes url. example "http://localhost:8080/download/naam.jpg"
-            String url = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFromDB/").path(name).toUriString();
-
-            String contentType = file.getContentType();
-
-            FileUploadResponseDto response = new FileUploadResponseDto(name, contentType, url);
-
-            uploadResponseList.add(response);
-        });
-        return uploadResponseList;
-    }
-
-    public void getZipDownload(String[] files, HttpServletResponse response) throws IOException {
-        try (ZipOutputStream zos = new ZipOutputStream(response.getOutputStream())) {
-            Arrays.stream(files).forEach(file -> {
-                try {
-                    createZipEntry(file, zos);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-
-            zos.finish();
-
-            response.setStatus(200);
-            response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;fileName=zipfile");
-        }
-    }
-
-    public Resource downLoadFileDatabase(String fileName) {
-
-        String url = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFromDB/").path(fileName).toUriString();
-
-        Resource resource;
-
-        try {
-            resource = new UrlResource(url);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Issue in reading the file", e);
-        }
-
-        if(resource.exists()&& resource.isReadable()) {
-            return resource;
-        } else {
-            throw new RuntimeException("the file doesn't exist or not readable");
-        }
-    }
-
-    public void createZipEntry(String file, ZipOutputStream zos) throws IOException {
-
-        Resource resource = downLoadFileDatabase(file);
-        ZipEntry zipEntry = new ZipEntry(Objects.requireNonNull(resource.getFilename()));
-        try {
-            zipEntry.setSize(resource.contentLength());
-            zos.putNextEntry(zipEntry);
-
-            StreamUtils.copy(resource.getInputStream(), zos);
-
-            zos.closeEntry();
-        } catch (IOException e) {
-            System.out.println("some exception while zipping");
-        }
     }
 }
